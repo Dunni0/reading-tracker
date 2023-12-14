270a00c799a987d8b20ed711332424279b51aa0d
Dunni <pelumioladunni3@gmail.com>
2023-12-14 13:15:40 +0100

    add image

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import Booklist from './components/Booklist';
-import Button from './components/Button';
+// import Button from './components/Button';
 import Header from './components/Header';
 import "./index.css"
  
 import BookContextProvider from './context/BookContext';
-import BookDeets from './components/BookDeets';
+// import BookDeets from './components/BookDeets';
 import BookForm from './components/BookForm';
 
 function App() {
   return (
     <div className="App">
+      <img src='unnamed.png' alt='cats' />
       <BookContextProvider> 
          <Header />
          <Booklist />
